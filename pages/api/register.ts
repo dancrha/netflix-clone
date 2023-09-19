96044707fe58eeb0403aba7efe19ae3df258eb95
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-19 17:47:22 -0400

    auth done.

---
+++ pages/api/register.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+import { isTryStatement } from "typescript";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return;
+  }
+  try {
+    const { email, name, password } = req.body;
+    const existingUser = await prismadb.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return res.status(422).json({ error: "Email taken." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).end();
+  }
+}
