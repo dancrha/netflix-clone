0b3d9db6ad571b20f95953ce156ee78c6557e157
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-20 22:41:20 -0400

    billboard done

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -4,8 +4,8 @@ import { getServerSession } from "next-auth";
 import prismadb from "@/lib/prismadb";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-const serverAuth = async (req: NextApiRequest) => {
-  const session = await getServerSession(req);
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session?.user?.email) {
     throw new Error("Not signed in");
--- pages/api/random.ts
+++ pages/api/random.ts
@@ -6,15 +6,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).end();
-  }
-
   try {
-    await serverAuth(req);
+    if (req.method !== "GET") {
+      return res.status(405).end();
+    }
 
-    const movieCount = await prismadb.movie.count();
-    const randomIndex = Math.floor(Math.random() * movieCount);
+    await serverAuth(req, res);
+
+    const moviesCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
 
     const randomMovies = await prismadb.movie.findMany({
       take: 1,
@@ -24,6 +24,7 @@ export default async function handler(
     return res.status(200).json(randomMovies[0]);
   } catch (error) {
     console.log(error);
-    return res.status(400).end();
+
+    return res.status(500).end();
   }
 }
