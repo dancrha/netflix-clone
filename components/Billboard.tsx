12db3c647731506317121cad029520e96da6bbd8
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-20 22:23:37 -0400

    billboard started

---
+++ components/Billboard.tsx
@@ -0,0 +1,10 @@
+import useBillboard from "@/hooks/useBillboard";
+import React from "react";
+
+const Billboard = () => {
+  const { data } = useBillboard();
+
+  return <div></div>;
+};
+
+export default Billboard;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,31 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
 import MobileMenu from "./MobileMenu";
 import AccountMenu from "./AccountMenu";
 import NavbarItem from "./NavbarItem";
+
+const TOP_OFFSET = 66;
+
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((current) => !current);
@@ -17,7 +37,11 @@ const Navbar = () => {
 
   return (
     <nav className='w-full fixed z-40'>
-      <div className='px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90'>
+      <div
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
         <img className='h-4 lg:h-7' src='/images/logo.png' alt='Logo' />
         <div className='flex-row ml-8 gap-7 hidden lg:flex'>
           <NavbarItem label='Home' />
