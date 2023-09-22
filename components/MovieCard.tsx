20c77839f6a8b1c1add75130600633ba477d0c90
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-22 16:50:02 -0400

    favorites list done

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
+import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -27,6 +28,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             >
               <BsFillPlayFill size={25} />
             </div>
+            <FavoriteButton movieId={data?.id} />
           </div>
           <p className='text-green-400 font-semibold mt-4'>
             New <span className='text-white'>2023</span>
