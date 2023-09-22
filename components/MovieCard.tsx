6f5c8e34161fdb70cbe3f74a897f1139cdafdbb2
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-22 17:13:07 -0400

    movie card/play button work

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/router";
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
   return (
     <div className='group bg-zinc-900 col-span relative h-[12vw]'>
       <img
@@ -23,7 +25,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         <div className='z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md'>
           <div className='flex flex-row items-center gap-3'>
             <div
-              onClick={() => {}}
+              onClick={() => router.push(`/watch/${data?.id}`)}
               className='cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transiton hover:bg-neutral-300'
             >
               <BsFillPlayFill size={25} />
