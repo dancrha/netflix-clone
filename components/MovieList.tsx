7bee0caa0e589302987c05b0380b3adc6b9fb880
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-22 15:53:04 -0400

    movie card progress

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { isEmpty } from "lodash";
+import MovieCard from "./MovieCard";
 
 interface MovieListProps {
   data: Record<string, any>;
@@ -18,7 +19,7 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
         </p>
         <div className='grid grid-cols-4 gap-2'>
           {data.map((movie) => (
-            <div key={movie.id}>movie</div>
+            <MovieCard key={movie.id} data={movie} />
           ))}
         </div>
       </div>
