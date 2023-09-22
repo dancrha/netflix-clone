2d463b615e852b942686f5033a8a0835225c1944
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-22 15:36:35 -0400

    movie list started

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
@@ -21,10 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   );
 }
